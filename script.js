e9c7f1dd7cbaeaeaca8bb5b10f668b7abfeec8a8
Cristian Gutierrez <cris123.gut@gmail.com>
2022-04-28 17:16:52 -0500

    Cambiando el script

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const names = ["Alice", "Bob", "Tiff", "Bruce", "Alice"];
 let countedNames = names.reduce(function (allNames, name) {
   if (name in allNames) {
     allNames[name]++;
-  } else {
-    allNames[name] = 1;
   }
   return allNames;
 }, {});
 // countedNames is:
 // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+function suma() {
+  return 2 + 2;
+}
